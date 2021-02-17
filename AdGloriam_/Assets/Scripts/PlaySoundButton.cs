f687ef4150fdb1a91c2c94e7f568dde5487e015e
Tommolo <72791771+Tommolo@users.noreply.github.com>
2021-02-17 16:09:41 +0100

    UlterioriModifiche

---
+++ AdGloriam_/Assets/Scripts/PlaySoundButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundButton : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip hover;
+    public AudioClip click;
+    public void loadNextScene()
+    {
+        StartCoroutine(LoadingLevel());
+    }
+    IEnumerator LoadingLevel()
+    {
+        yield return new WaitForSeconds(1f);
+        Application.LoadLevel("OtherScene");
+    }
+    public void Onhover()
+    {
+        source.PlayOneShot(hover);
+    }
+    public void OnClick()
+    {
+        source.PlayOneShot(click);
+    }
+
+}

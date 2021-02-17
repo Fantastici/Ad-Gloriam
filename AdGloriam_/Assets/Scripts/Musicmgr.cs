9815845deb6259d1d7b6d0d22642eb5273b03ea8
Tommolo <72791771+Tommolo@users.noreply.github.com>
2021-02-17 11:27:33 +0100

    MenuDefinitivo

---
+++ AdGloriam_/Assets/Scripts/Musicmgr.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Musicmgr : MonoBehaviour
+{
+    public AudioSource audio;
+    // Start is called before the first frame update      
+    void Start() { }
+    // Update is called once per frame      
+    void Update() { }
+    public void Music()
+    {
+        audio.Play();
+        Debug.Log("play");
+    }
+
+}

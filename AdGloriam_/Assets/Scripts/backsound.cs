f074d78b117e157b046ba4367755486dac5ea6ff
Tommolo <72791771+Tommolo@users.noreply.github.com>
2021-02-17 17:50:03 +0100

    ciao

--- AdGloriam_/Assets/Scripts/backsound.cs
+++ AdGloriam_/Assets/Scripts/backsound.cs
@@ -9,13 +9,7 @@ public class backsound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (GameObject.Find("backsound on")==null)
-        {
-
-            GetComponent<AudioSource>().Play();
-            gameObject.name = "backsound on";
-           
-        }
+       
     }
 
     // Update is called once per frame

77fa95762996fd5acd82ce764493bbbd5eb97b1f
Tommolo <72791771+Tommolo@users.noreply.github.com>
2021-02-18 11:42:54 +0100

    Ulteriori Modifiche su Menu

---
+++ AdGloriam_/Assets/MenuScript/StartMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+  public void exit()
+    {
+        Application.Quit();
+    }
+    public void play(string nome_scena)
+    {
+        Application.LoadLevel(nome_scena);
+    }
+}

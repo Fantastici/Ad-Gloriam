1332efaeb5bf07d66d3ae6283e9506941c924d09
giangiopiz8 <73116583+giangiopiz8@users.noreply.github.com>
2021-02-18 13:26:49 +0100

    scudo piu sound scena

---
+++ AdGloriam_/Assets/Scripts/OstacoliEScudo/scudoCollisione.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scudoCollisione : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            SoundManagerScript.PlaySound("scudoSound");
+            Destroy(gameObject);
+        }
+    }
+}

8628d112db567603da2538e9d17a4978a76993d7
Tommolo <72791771+Tommolo@users.noreply.github.com>
2021-02-19 16:34:20 +0100

    Button sound & script

--- AdGloriam_/Assets/UI/Scripts/ClickPauseMenu.cs
+++ AdGloriam_/Assets/UI/Scripts/ClickPauseMenu.cs
@@ -4,10 +4,45 @@ using UnityEngine;
 
 public class ClickPauseMenu : MonoBehaviour
 {
-    public GameObject pauseMenu;
-    public void ClickPause()
+  
+ 
+    public GameObject PauseScreen;
+    public GameObject PauseButton;
+
+    bool GamePaused;
+
+
+    // Start is called before the first frame update
+    void Start()
     {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0;
+        GamePaused = false;
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GamePaused)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+
+    public void PauseGame()
+    {
+        GamePaused = true;
+        PauseScreen.SetActive(true);
+        PauseButton.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        GamePaused = false;
+        PauseScreen.SetActive(false);
+        PauseButton.SetActive(true);
+        }
+    public void sound_volume(float volume)
+    {
+        PlayerPrefs.SetFloat("volume", volume);
+    }
+    
 }

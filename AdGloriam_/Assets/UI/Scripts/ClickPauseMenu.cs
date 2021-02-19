8f69880974fc70a7ebd2d6f123ee98a29a7d4e06
Tommolo <72791771+Tommolo@users.noreply.github.com>
2021-02-19 13:47:58 +0100

    tasto pausa Con resume e home

---
+++ AdGloriam_/Assets/UI/Scripts/ClickPauseMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickPauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public void ClickPause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+}

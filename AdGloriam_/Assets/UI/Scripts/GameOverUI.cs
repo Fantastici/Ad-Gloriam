b0dd871f9d3e24943e97313ce2fc3308b0cdc478
Tommolo <72791771+Tommolo@users.noreply.github.com>
2021-03-04 11:02:52 +0100

    Share Script and Button

--- AdGloriam_/Assets/UI/Scripts/GameOverUI.cs
+++ AdGloriam_/Assets/UI/Scripts/GameOverUI.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class GameOverUI : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject GameOver;
+	public string shareMessage;
    public void Home(string nome_scena)
     {
         SceneManager.LoadScene(nome_scena);
@@ -16,6 +18,30 @@ public class GameOverUI : MonoBehaviour
         SceneManager.LoadScene(nome_scena);
         GameOver.SetActive(false);
     }
-  
-   
+
+	public void Share()
+    {
+		shareMessage= "Ciao il mio score è di " + FindObjectOfType<score>().PunteggioFinale() + " Punti!";
+
+		StartCoroutine(TakeScreenshotAndShare());
+
+	}
+	private IEnumerator TakeScreenshotAndShare()
+	{
+		yield return new WaitForEndOfFrame();
+
+		Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+		ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+		ss.Apply();
+
+		string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
+		File.WriteAllBytes(filePath, ss.EncodeToPNG());
+
+	
+		Destroy(ss);
+
+		new NativeShare().AddFile(filePath).SetSubject("Ad Gloriam").SetText(shareMessage).Share();
+
+	}
+
 }

704025a9bc5080397d78cc1f13d7c17a3e028c06
Tommolo <72791771+Tommolo@users.noreply.github.com>
2021-03-24 23:14:16 +0100

    prima bozza tutorial

---
+++ AdGloriam_/Assets/UI/Scripts/TutorialManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpindex;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (popUpindex == 0)
+        {
+            popUps[0].SetActive(true);
+            if (Input.GetMouseButtonDown(0))
+            {
+                popUps[0].SetActive(false);
+                popUpindex++;
+
+            }
+        }
+        /* if (popUpindex == 1)
+         {
+             popUps[0].SetActive(true);
+             if (FindObjectOfType<ViewAcolumn>().SeeColumn())
+             {
+                 popUps[0].SetActive(false);
+                 popUpindex++;
+
+             }
+         }
+    */
+
+    }
+}
+

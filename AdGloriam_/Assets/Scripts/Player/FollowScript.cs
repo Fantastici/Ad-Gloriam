e0e3f20dc95016f6af39a2af4a8a7180bd0a9c75
ErikSergio <73118438+ErikSergio@users.noreply.github.com>
2021-05-03 23:15:25 +0200

    Camera

---
+++ AdGloriam_/Assets/Scripts/Player/FollowScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 pos;
+    public float offset;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        pos.y = target.position.y + offset;
+
+        transform.position = pos;
+    }
+
+    
+}
